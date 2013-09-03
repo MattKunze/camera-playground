d46ac364ff28b30d98c13dc8d204e3b2812b0d48
Matt Kunze <matt.kunze@datasplice.com>
2013-09-03 15:18:18 -0600

    basic cordova and input photo input is working

--- platforms/android/src/com/DataSplice/CameraPlayground/CameraPlayground.java
+++ platforms/android/src/com/DataSplice/CameraPlayground/CameraPlayground.java
@@ -17,7 +17,7 @@
        under the License.
  */
 
-package com.DataSplice.CameraPlayground;
+package com.datasplice.CameraPlayground;
 
 import android.os.Bundle;
 import org.apache.cordova.*;
--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+'use strict;'
+
+var photos = [];
 var app = {
   // Application Constructor
   initialize: function() {
@@ -29,19 +32,179 @@ var app = {
     var doc = $(document);
     doc.ready(this.onDocumentReady);
     doc.on('deviceready', this.onDeviceReady);
+    doc.on('click', '.capture-input', this.captureInput);
+    doc.on('click', '.capture-cordova', this.captureCordova);
+    doc.on('click', '.capture-getusermedia', this.captureGetUserMedia);
+    doc.on('click', '.dropdown-menu .select', this.selectMenuItem);
+    doc.on('click', '.select-photo', this.selectPhoto);
+    doc.on('click', '.delete-photo', this.deletePhoto);
   },
   onDocumentReady: function() {
-    app.receivedEvent('ready');
+    app.log('Received event', 'ready');
+
+    var json = localStorage.getItem('photos');
+    if(json) {
+      photos = JSON.parse(json);
+      for(var index = 0; index < photos.length; index++) {
+        app.addPhoto(photos[index], { silent: true, index: index });
+      }
+    }
   },
   onDeviceReady: function() {
-    app.receivedEvent('deviceready');
+    app.log('Received event', 'deviceready');
   },
-  // Update DOM on a Received Event
-  receivedEvent: function(id) {
-    console.log('Received Event: ' + id);
+  captureInput: function(ev) {
+    var target = $(ev.target).closest('tr');
+    target.find('input[type=file]').remove();
+    var input = $('<input class="hidden" type="file" accept="image/*;capture=camera">');
+    input
+      .appendTo(target)
+      .on('change', function(ev) {
+        var file = ev.target.files[0];
+        if(file) {
+          var reader = new FileReader;
+          reader.onload = function() {
+            app.addPhoto(reader.result);
+          }
+          reader.readAsDataURL(file);
+        }
+      })
+      .click();
+  },
 
+  captureCordova: function(ev) {
+    try {
+      var options = {}
+
+      var destination = app.getOption('.cordova-destination');
+      options.destinationType = Camera.DestinationType[destination];
+
+      var encoding = app.getOption('.cordova-encoding');
+      options.encodingType = Camera.EncodingType[encoding];
+
+      var direction = app.getOption('.cordova-direction');
+      options.cameraDirection = Camera.Direction[direction];
+
+      app.log('Cordova Options', JSON.stringify(options));
+      navigator.camera.getPicture(app.cordovaSuccess, app.cordovaError, options);
+    }
+    catch (error) {
+      app.log('Cordova Exception', error.message);
+    }
+  },
+  cordovaSuccess: function(data) {
+    var message;
+    if(!data.match(/^file:\/\//)) {
+      data = 'data:image/jpeg;base64,' + data;
+      message = data.substring(0, 100);
+    }
+    else {
+      message = data;
+    }
+
+    app.log('Cordova Success', message);
+    app.addPhoto(data);
+  },
+  cordovaError: function(message) {
+    app.log('Cordova Error', message);
+  },
+
+  captureGetUserMedia: function(ev) {
+    console.warn('capture getusermedia')
+  },
+  selectMenuItem: function(ev) {
+    var target = $(ev.target);
+    target.closest('.btn-group').find('span.current').text(target.text());
+  },
+  getOption: function(selector) {
+    return $(selector).text().toUpperCase().replace(/ /g, '_');
+  },
+  addPhoto: function(data, options) {
+    try {
+      var photo;
+      if(_.isString(data)) {
+        photo = { data: data };
+
+        var match = data.match(/data:([^;]*);base64,(.*)/);
+        if(match) {
+          photo.contentType = match[1];
+          photo.fileSize = (match[2].length / 4) * 3;
+          photo.label = match[2].substring(0, 25) + '...'
+
+          app.log('Data Photo', 'Size: ' + photo.fileSize + ' Type: ' + photo.contentType);
+        }
+        else {
+          photo.contentType = 'image/' + data.match(/.(\w+)$/)[1];
+          photo.fileSize = '?';
+        }
+        photos.push(photo);
+        localStorage.setItem('photos', JSON.stringify(photos));
+      }
+      else {
+        photo = data;
+      }
+
+      var table = $('.results.panel table');
+      table.find('tr').removeClass('success');
+
+      var index = photos.length - 1;
+      if(options && (options.index !== undefined)) {
+        index = options.index;
+      }
+      var row = $('<tr>')
+        .data('photo', index)
+        .append($('<td>')
+          .append(
+            $('<a href="" class="select-photo"><i class="glyphicon glyphicon-bookmark"/> </a>')
+              .append(photo.label || photo.data)
+          )
+        )
+        .append($('<td>').text(photo.fileSize))
+        .append($('<td>').text(photo.contentType))
+        .append($('<td><a href="" class="delete-photo"><i class="glyphicon glyphicon-trash"/></a></td>'))
+        .appendTo(table);
+
+      if(!options || !options.silent) {
+        row.addClass('success');
+        $('.results.panel img').attr('src', photo.data);
+      }
+    }
+    catch (error) {
+      app.log('Caught Exception', error.message);
+    }
+  },
+  selectPhoto: function(ev) {
+    var row = $(ev.target).closest('tr');
+    if(!row.hasClass('success')) {
+      var offset = row.data('photo');
+      $('.results.panel img').attr('src', photos[offset].data);
+
+      var table = $('.results.panel table');
+      table.find('tr').removeClass('success');
+
+      row.addClass('success');
+    }
+
+    return false;
+  },
+  deletePhoto: function(ev) {
+    var row = $(ev.target).closest('tr');
+    var offset = row.data('photo');
+    photos.splice(offset, 1);
+    localStorage.removeItem('photos');
+    localStorage.setItem('photos', JSON.stringify(photos));
+
+    if(row.hasClass('success')) {
+        $('.results.panel img').attr('src', '');
+    }
+    row.remove();
+
+    return false;
+  },
+  log: function(key, message) {
     $('<li class="list-group-item">')
-      .text('Received Event: ' + id)
-      .appendTo($('.results.panel .list-group'));
+      .text(key + ': ' + message )
+      .prependTo($('.log.panel .list-group'));
   }
 };
+
